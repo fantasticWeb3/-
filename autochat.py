d61d74bed32766334199cc3ad5a5bfafa0dcf338
fantasticWeb3 <108315541+fantasticWeb3@users.noreply.github.com>
2022-06-28 12:47:49 +0800

    Update autochat.py

--- autochat.py
+++ autochat.py
@@ -1,1 +1,32 @@
-wechat auto chat
+from __future__ import unicode_literals
+
+import wechat
+import json
+import time
+from wechat import WeChatManager, MessageType
+
+wechat_manager = WeChatManager(libs_path='../../libs')
+
+
+# 这里测试函数回调
+@wechat.CONNECT_CALLBACK(in_class=False)
+def on_connect(client_id):
+    print('[on_connect] client_id: {0}'.format(client_id))
+
+
+@wechat.RECV_CALLBACK(in_class=False)
+def on_recv(client_id, message_type, message_data):
+    print('[on_recv] client_id: {0}, message_type: {1}, message:{2}'.format(client_id,
+                                                                            message_type, json.dumps(message_data)))
+
+if __name__ == "__main__":
+    bot = LoginTipBot()
+
+    # 添加回调实例对象
+    wechat_manager.add_callback_handler(bot)
+    wechat_manager.manager_wechat(smart=True)
+
+    # 阻塞主线程
+    while True:
+        time.sleep(0.5)
+
